ac18922b1887a077b5244aa9646e2c526b921f7a
Hakan Terzioglu <hakanterzioglu0101@gmail.com>
2025-09-20 21:04:56 +0300

    react search agent

---
+++ hello.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain import hub
+from langchain.agents.react.agent import create_react_agent
+from langchain.agents import AgentExecutor
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model='gpt-4')
+react_prompt=hub.pull('hwchase17/react')
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt,
+)
+
+agent_executor=AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain=agent_executor
+
+
+def main():
+    response = chain.invoke(
+        input={
+            'input':'search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details'
+        }
+    )
+    print(response)
+
+
+if __name__ == "__main__":
+    main()
